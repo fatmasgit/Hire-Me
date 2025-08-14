928e97faed108b35996db07218a22fcafc6944f8
fatmasgit <accfatma5@gmail.com>
2025-08-14 18:16:55 +0300

    replacing old folders on the repo

--- src/Pages/CandidateLogIn.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LogIn from '../components/candidates/LogIn'
-
-export default function CandidateLogInPage() {
-  return (
-    <LogIn />
-  )
-}
--- src/Pages/CandidateSignUp.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import SignUp from '../components/candidates/SignUp'
-
-export default function CandidateSignUpPage() {
-  return (
-    <SignUp />
-  )
-}
